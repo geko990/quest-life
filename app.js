e5a79e9405816114caf7f28477e10f383f176231
Enrico Gargiulo <enrico@192.168.1.67>
2026-02-11 16:21:48 +0100

    chore: bump version to v3.1.1 (theme fix + swipe fix)

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-console.log("APP.JS LOADED - v3.1.0");
+console.log("APP.JS LOADED - v3.1.1");
 /* ============================================
    QUEST LIFE - RPG Habit Tracker v2
    Main Application Script
    ============================================ */
 
-const APP_VERSION = '3.1.0';
-import { DEFAULT_ATTRIBUTES, DEFAULT_ABILITIES, AVATAR_EMOJIS, ACCENT_COLORS, XP_CONFIG, TITLES, DAY_NAMES, CHALLENGE_TEMPLATES } from './js/modules/constants.js?v=3.1.0';
-import { state, setState, updateState, loadState, saveState, resetAll } from './js/modules/state.js?v=3.1.0';
-import { getGameDateObj, formatISO, getGameDate, getGameDateString, getWeekIdentifier, getMonthIdentifier, getYearIdentifier, calculateXp, getXpForLevel, ensureUniqueIds, getCumulativeXpForLevel, calculateLevelFromXp, formatDate, generateId } from './js/modules/utils.js?v=3.1.0';
-import { setFileHandle, getFileHandle, linkDatabaseFile as linkDBInit, loadFileHandleOnStart, updateDbStatusUI, saveDataToFile } from './js/modules/storage.js?v=3.1.0';
+const APP_VERSION = '3.1.1';
+import { DEFAULT_ATTRIBUTES, DEFAULT_ABILITIES, AVATAR_EMOJIS, ACCENT_COLORS, XP_CONFIG, TITLES, DAY_NAMES, CHALLENGE_TEMPLATES } from './js/modules/constants.js?v=3.1.1';
+import { state, setState, updateState, loadState, saveState, resetAll } from './js/modules/state.js?v=3.1.1';
+import { getGameDateObj, formatISO, getGameDate, getGameDateString, getWeekIdentifier, getMonthIdentifier, getYearIdentifier, calculateXp, getXpForLevel, ensureUniqueIds, getCumulativeXpForLevel, calculateLevelFromXp, formatDate, generateId } from './js/modules/utils.js?v=3.1.1';
+import { setFileHandle, getFileHandle, linkDatabaseFile as linkDBInit, loadFileHandleOnStart, updateDbStatusUI, saveDataToFile } from './js/modules/storage.js?v=3.1.1';
 
 // Expose globals for HTML event handlers and legacy code
 window.state = state;
--- js/modules/state.js
+++ js/modules/state.js
@@ -6,7 +6,7 @@ import { DEFAULT_ATTRIBUTES, DEFAULT_ABILITIES } from './constants.js';
 import { getMonthIdentifier, getGameDate, ensureUniqueIds, getGameDateString } from './utils.js';
 import { saveDataToFile } from './storage.js';
 
-export const APP_VERSION = '3.0.0';
+export const APP_VERSION = '3.1.1';
 
 export let state = {
     player: {
