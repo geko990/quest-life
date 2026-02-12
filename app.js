eee8540687ffb1e1d06c51befa7328559dc599da
Enrico Gargiulo <enrico@192.168.1.67>
2026-02-12 02:02:26 +0100

    Hotfix: Fix ReferenceError in app.js and bump version to v3.1.15

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("APP.JS LOADED - v3.1.14");
    Main Application Script
    ============================================ */
 
-const APP_VERSION = '3.1.14';
+const APP_VERSION = '3.1.15';
 import { DEFAULT_ATTRIBUTES, DEFAULT_ABILITIES, AVATAR_EMOJIS, ACCENT_COLORS, XP_CONFIG, TITLES, DAY_NAMES, CHALLENGE_TEMPLATES } from './js/modules/constants.js?v=3.1.14';
 import { state, setState, updateState, loadState, saveState, resetAll, checkHealthRollover } from './js/modules/state.js?v=3.1.14';
 import { getGameDateObj, formatISO, getGameDate, getGameDateString, getWeekIdentifier, getMonthIdentifier, getYearIdentifier, calculateXp, getXpForLevel, ensureUniqueIds, getCumulativeXpForLevel, calculateLevelFromXp, formatDate, generateId } from './js/modules/utils.js?v=3.1.14';
@@ -6916,12 +6916,6 @@ window.closeExerciseManager = closeExerciseManager;
 window.saveExerciseToDatabase = saveExerciseToDatabase;
 window.deleteExerciseFromDatabase = deleteExerciseFromDatabase;
 window.openExerciseLogModal = openExerciseLogModal;
-
-// Custom input handlers
-window.confirmManualIntake = confirmManualIntake;
-window.confirmNewGoal = confirmNewGoal;
-window.confirmManualBurn = confirmManualBurn;
-window.confirmHealthSteps = confirmHealthSteps;
 
 // Tasks & Progress
 window.toggleHabit = toggleHabit;
