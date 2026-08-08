245081a5e2d2bbd540929a8ae77ab17ccabda3d5
Enrico Gargiulo <enrico@192.168.1.74>
2026-08-08 16:01:50 +0200

    v5.8.32: Bump version to notify PWA of available update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // RPG LIFE SERVICE WORKER
-const CACHE_NAME = 'rpg-life-v5.8.31-1786146909923';
-const SW_VERSION = '5.8.31';
+const CACHE_NAME = 'rpg-life-v5.8.32-1786197700494';
+const SW_VERSION = '5.8.32';
 
 // Assets to precache on install (app shell)
 const PRECACHE_ASSETS = [
