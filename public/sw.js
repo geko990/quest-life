7a7b1a8eb1929701530688878e67d7bec2550504
Enrico Gargiulo <enrico@192.168.1.81>
2026-08-27 16:41:25 +0200

    release v5.9.160: risolto taglio/sbiadimento parte superiore icone header tramite dimensionamento e overflow

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // RPG LIFE SERVICE WORKER
-const CACHE_NAME = 'rpg-life-v5.9.159-1787841436994';
-const SW_VERSION = '5.9.159';
+const CACHE_NAME = 'rpg-life-v5.9.160-1787841682810';
+const SW_VERSION = '5.9.160';
 
 // Assets to precache on install (app shell)
 const PRECACHE_ASSETS = [
